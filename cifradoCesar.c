3e9eb8d92940d4636dbf3e90de2a60075b27faab
katearzate <61952699+katearzate@users.noreply.github.com>
2021-04-12 11:29:17 -0500

    Implementacion de desencriptar

--- cifradoCesar.c
+++ cifradoCesar.c
@@ -14,22 +14,22 @@
 #define inicio_ascii_mayusculas 65
 #define inicio_ascii_minusculas 97
 #define MOD(i, n) (i % n + n) % n //modulo positivo
-
+			      
 const char *alfMinusculas = "abcdefghijklmnopqrstuvwxyz",
            *alfMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 void encriptar(char *mensaje, char *resultado, int saltos);
-//void desencriptar(char mensaje[max],int saltos);
+void desencriptar(char *mensaje, char *resultado,int saltos);
 
 
 
-//Funcion para cifrar el mensaje
 void encriptar(char *mensaje, char *resultado, int saltos) {
+  //printf("El mensaje encriptado es: ");
   int i = 0;
   while (mensaje[i]) {
     char caracterActual = mensaje[i];
-    int posicionOriginalAscii = (caracterActual);	//ord: numero de codigo ascii del caracter
+    int posicionOriginalAscii = (caracterActual);	
     if (!isalpha(caracterActual)) {		//isalpha: corresponde a alguna letra
       resultado[i] = caracterActual;
       i++;
@@ -48,25 +48,55 @@ void encriptar(char *mensaje, char *resultado, int saltos) {
                               saltos) %
                              long_alfabeto];
     }
+    
+    //printf("%c", (resultado[i]));
+    i++;
+  }
+}
+
+void desencriptar(char *mensaje, char *resultado, int saltos) {
+  int i = 0;
+  while (mensaje[i]) {
+    char caracterActual = mensaje[i];
+    int posicionOriginalAscii = (caracterActual);
+    if (!isalpha(caracterActual)) {
+      resultado[i] = caracterActual;
+      i++;
+      continue; 
+    }
+    if (isupper(caracterActual)) {		//es mayuscula
+      resultado[i] = alfMayusculas[MOD(
+          posicionOriginalAscii - inicio_ascii_mayusculas - saltos,
+          long_alfabeto)];
+    } else if (islower(caracterActual)) { 	//es minuscula
+      resultado[i] = alfMinusculas[MOD(
+          posicionOriginalAscii - inicio_ascii_minusculas - saltos,
+          long_alfabeto)];
+    }
     i++;
   }
 }
 
 int main(){
-	char mensaje[max], mensajeCifrado[max];
+	char mensaje[max], mensajeCifrado[max], mensajeDescifrado[max];
 	int saltos;
 	
 	printf(" ********** CIFRADO DE CESAR ************ \n");
-	
-	printf("Escribe un mensaje para cifrar: \n");
+	printf("Escribe el mensaje para cifrar: ");
 	fgets(mensaje, max, stdin);
-    mensaje[strcspn(mensaje, "\r\n")] = 0;		//strcspn: devuelve la longitud de la cadena 
-	
-	printf("Número de saltos: \n");
+    mensaje[strcspn(mensaje, "\r\n")] = 0;		//strcspn: devuelve la longitud de la cadena 	
+	printf("Número de saltos: ");
 	scanf("%d", &saltos);
 	
-	printf("El mensaje original es: %s\n", mensaje);
 	encriptar(mensaje, mensajeCifrado, saltos);
+	printf("\nEl mensaje encriptado es: %s\n", mensajeCifrado);
 	
-	//printf("%d", mensaje[0]); //muestra codigo ascii
+    desencriptar(mensaje, mensajeDescifrado, saltos);
+    printf("\nEl mensaje desencriptado es: %s\n", mensajeDescifrado);
+	
+    
+    //printf("%d", mensaje[0]); //muestra codigo ascii
+	printf("\n * \n * \n * \n\n");
+	main();
+	
 }
